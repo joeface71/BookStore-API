b78c627d82c0d55c716ea35d034cf800a120fd9d
joeface71 <joefacenda@yahoo.com>
2020-09-27 16:26:38 -0400

    Set up get endpoints

--- BookStore-API/Controllers/HomeController.cs
+++ BookStore-API/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace BookStore_API.Controllers
+namespace BookStore_API.Contracts
 {   
     /// <summary>
     /// This is a bookstore api controller
