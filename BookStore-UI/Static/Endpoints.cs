e1fafe028cd9e91e9b0504c146d4a661857ed487
joeface71 <joefacenda@yahoo.com>
2020-11-06 17:12:44 -0500

    UI endpoints created

---
+++ BookStore-UI/Static/Endpoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_UI.Static
+{
+    public static class Endpoints
+    {
+        public static string BaseUrl = "http://localhost:44302/";
+        public static string AuthorsEndPoint = $"{BaseUrl}api/authors/"; 
+        public static string BooksEndPoint = $"{BaseUrl}api/books/"; 
+    }
+}
