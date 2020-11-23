a711d4ff4838545c7f0a0a1d63b2b6f65b16c1b9
joeface71 <joefacenda@yahoo.com>
2020-11-23 16:22:23 -0500

    Authentication

---
+++ BookStore-UI/Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace BookStore_UI.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(15, ErrorMessage = "Your password is limited to {2} to {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class LoginModel
+    {
+
+    }
+
+   
+}
--- BookStore-UI/Static/Endpoints.cs
+++ BookStore-UI/Static/Endpoints.cs
@@ -7,8 +7,9 @@ namespace BookStore_UI.Static
 {
     public static class Endpoints
     {
-        public static string BaseUrl = "http://localhost:44302/";
+        public static string BaseUrl = "https://localhost:44302/";
         public static string AuthorsEndPoint = $"{BaseUrl}api/authors/"; 
-        public static string BooksEndPoint = $"{BaseUrl}api/books/"; 
+        public static string BooksEndPoint = $"{BaseUrl}api/books/";
+        public static string RegisterEndPoint = $"{BaseUrl}api/users/register/";
     }
 }
